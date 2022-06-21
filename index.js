930a8104f530c6710947a3128fb389db617e2f7e
Ishtjotkaur <ishtjotkaurgill1664@gmail.com>
2022-06-21 19:39:31 +0530

    changes made

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ export default class index extends Component {
       <div>
         <h1>This is my heading</h1>
         <p>here i am goint to write some info</p>
+        <p>Now i am making here more changes</p>
       </div>
     )
   }
