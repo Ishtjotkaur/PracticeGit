24426a7f8babecf4ec9e4593615c6909d0e267e2
Mehak <Mehak@123gmail.com>
2022-06-22 12:23:24 +0530

    12change

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ export default class index extends Component {
         <p>Now i am making here more changes</p>
         <p>1st insertion done by 1st person</p>
         <p>Now changes are made from another side</p>
-        <p>sugar sweet</p>
+        <p>sugar daddy</p>
         <p>2nd made a change when both users working at the same time</p>
       </div>
     )
